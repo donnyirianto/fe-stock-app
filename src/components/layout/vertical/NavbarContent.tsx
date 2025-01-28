eaedb1d535eb9a09898ebf5c7eeb1fac9027d0f5
zaranggi <donnyirianto.anggriawan@gmail.com>
2025-01-29 05:09:45 +0700

    update

--- src/components/layout/vertical/NavbarContent.tsx
+++ src/components/layout/vertical/NavbarContent.tsx
@@ -11,11 +11,14 @@ import UserDropdown from '@components/layout/shared/UserDropdown'
 // Util Imports
 import { verticalLayoutClasses } from '@layouts/utils/layoutClasses'
 
+//import NavSearch from '@components/layout/shared/search'
+
 const NavbarContent = () => {
   return (
     <div className={classnames(verticalLayoutClasses.navbarContent, 'flex items-center justify-between gap-4 is-full')}>
       <div className='flex items-center gap-4'>
         <NavToggle />
+
         <ModeDropdown />
       </div>
       <div className='flex items-center'>
